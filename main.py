7349a3dc922ff271660a07a5bf8d10a4e7444e76
smrk007 <smk170630@utdallas.edu>
2021-05-28 16:19:45 -0500

    Added some credentials and started to integrate the api with discord bot.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-class ImperialBot():
-  def __init__(self):
-    pass
+import os
 
+import discord
+from dotenv import load_dotenv
 
-if __name__ == '__main__':
-  
-  bot = ImperialBot()
-  bot.start()
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+client.run(TOKEN)
