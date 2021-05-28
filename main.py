bcfdf8dcb0714c63a0f9de9ce418fc2facce9729
smrk007 <smk170630@utdallas.edu>
2021-05-28 17:24:10 -0500

    Added emoji reaction handling.

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ client = discord.Client()
 @client.event
 async def on_message(message):
   if message.content.startswith("OFFICIAL PROPOSAL:"):
-    # Check if member is a senator
+
+    # Check if author is part of the Imperial Senate
     if 'ImperialSenator' not in message.author.roles and 'Emperor' not in message.author.roles:
       # Reject the Message
       await message.reply(content="You are not an Imperial Senator, so your proposal is immediately rejected.")
-      pass
-    pass
+      return
+
+@client.event
+async def on_reaction_add(reaction, user):
+  print(reaction.count)
+  print(reaction.emoji)
+  print(reaction.message.content)
 
 client.run(TOKEN)
